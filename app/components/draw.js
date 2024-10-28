6ec41b07efda5ca2d3f889910073d980ff35f055
Filli4 <145070135+Filli4@users.noreply.github.com>
2024-10-28 12:30:11 +0100

    deploy.1

--- app/components/draw.js
+++ app/components/draw.js
@@ -1,6 +1,6 @@
 export default function Draw() {
     return(
-         <div className="bg-green-800 text-3xl border-4 border-opacity-50 border-green-200 text-white shadow-xl shadow-zinc-400 font-sans justify-center flex font-extrabold p-4 rounded-lg w-52 ">It&apos; a DRAW! </div>
+         <div className="bg-green-800 text-3xl border-4 border-opacity-50 border-green-200 text-white shadow-xl shadow-zinc-400 font-sans justify-center flex font-extrabold p-4 rounded-lg w-52 ">{`It's a draw!`} </div>
     )
        
     
